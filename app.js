1ae3276915e02e61ac237e99183545164f6ae9e0
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2018-04-04 21:20:15 +0700

    update response

--- app.js
+++ app.js
@@ -80,28 +80,6 @@ function handleMessageEvent(event){
                                 "data": "Espresso"
                             }
                         ]
-                    },
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=222"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=222"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/222"
-                            }
-                        ]
                     }
                 ]
             }
